855d3fe1e0ff64f0cb591049cf726ced23a767a3
AlextdmAlert <75531771+AlextdmAlert@users.noreply.github.com>
2020-12-05 19:17:55 +0200

    Delete CalcArea.rb

--- CalcArea.rb
+++
@@ -1,6 +0,0 @@
-puts "Введите основание треугольника?"
-base = gets.chomp
-puts "Введите высоту треугольника?"
-height = gets.chomp
-area = 0.5*base.to_f*height.to_f
-puts "Площадь треугольника #{area}"

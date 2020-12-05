7fc83275fefd325a892ac6948baac12cee8d10ad
AlextdmAlert <75531771+AlextdmAlert@users.noreply.github.com>
2020-12-05 19:15:57 +0200

    Update CalcWeight.rb
    v1

--- Lesson 1/CalcWeight.rb
+++ Lesson 1/CalcWeight.rb
@@ -1,1 +1,10 @@
-
+puts "Как тебя зовут?"
+name = gets.chomp
+puts "Какой твой рост?"
+height = gets.chomp
+weight = (height.to_f-110)*1.15
+if (weight < 0) then
+   puts "#{name}, Ваш вес уже оптимальный"
+else
+   puts "#{name}, Ваш идеальный вес #{weight} кг."
+end

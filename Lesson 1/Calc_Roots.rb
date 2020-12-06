45dbf351c12a7ad842e2affc46b7f510852aab40
AlextdmAlert <75531771+AlextdmAlert@users.noreply.github.com>
2020-12-06 18:26:36 +0200

    Update Calc_Roots.rb

--- Lesson 1/Calc_Roots.rb
+++ Lesson 1/Calc_Roots.rb
@@ -7,14 +7,15 @@ b = gets.chomp.to_i
 puts 'Введите число c'
 c = gets.chomp.to_i
 
-d = b**2 - 4 * a * c
+d = b ** 2 - 4 * a * c
+r = Math.sqrt(d)
 
 if d < 0
-  puts "d= #{d}. Корней нет"
+  puts "d = #{d}. Корней нет"
 elsif d == 0
-  puts "d= #{d}. x1 = #{-b/(2 * a)}"
+  puts "d = #{d}. x1 = #{-b/(2 * a)}"
 else
-  puts "d= #{d}"
-  puts "x1 = #{(-b + Math.sqrt(d))/(2 * a)}"
-  puts "x2 = #{(-b - Math.sqrt(d))/(2 * a)}"
+  puts "d = #{d}"
+  puts "x1 = #{(-b + r)/(2 * a)}"
+  puts "x2 = #{(-b - r)/(2 * a)}"
 end
